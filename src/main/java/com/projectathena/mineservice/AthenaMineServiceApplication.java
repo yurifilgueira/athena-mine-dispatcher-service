73914033c2230da9b4f388f13666fc7671384062
yurifilgueira <yurimedeiros141@gmail.com>
2025-09-15 21:28:21 -0300

    feat: Add service to dispatch mining tasks

---
+++ src/main/java/com/projectathena/mineservice/AthenaMineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.projectathena.mineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaMineServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthenaMineServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectathena/mineservice/AthenaMineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectathena.mineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaMineServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
